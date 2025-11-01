221404dfa0e9ea19819affcb4aefab9220ae5e04
myrasfy <myrasfy.mp@gmail.com>
2025-11-01 14:11:18 +0300

    Added "Welcome" and temp load animation

---
+++ src/scripts/loadAnimation.js
@@ -0,0 +1,12 @@
+/* TEMP CHATGPT CODE */
+
+window.onload = function () {
+    const loader = document.getElementById('loadAnimation');
+
+    setTimeout(() => {
+        loader.style.opacity = '0';
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 1000);
+    }, 1000);
+};
